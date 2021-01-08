fb08949db709ee2ab77153f9e73b8e4ce32e9fc6
E-ludium <48441956+E-ludium@users.noreply.github.com>
2021-01-09 01:00:46 +0200

    Added database for managing data
    Added the database which the application will use to store the user's media files, as well as metadata for each of these files

--- main.py
+++ main.py
@@ -7,6 +7,8 @@
 from tkinter import *
 from tkinter import filedialog
 import configparser
+import sqlite3
+from sqlite3 import Error
 
 
 class SongStorage(Tk):  # The GUI class responsible for showing the interface to the user
@@ -14,10 +16,14 @@ class SongStorage(Tk):  # The GUI class responsible for showing the interface to
         super().__init__()
         self.title("Song Storage")
         
-        # Declaring the variables needed for back-end
+        """Declaring the variables needed for back-end"""
+        # Variables tasked with processing the configuration file of the application
         self.config = configparser.ConfigParser()  # We will use a config file to store the path of the media folder
         self.config.read('config.ini')
         self.media_folder = self.config['MEDIA FOLDER']['folder']  # The value that stores the path of the media folder
+
+        # Variable tasked with managing the database connected to the application
+        self.connection = self.connect_to_database()
         
         # Declaring the variables the GUI will use
         self.init_frame = Frame()  # The main frame of the application
@@ -28,8 +34,12 @@ class SongStorage(Tk):  # The GUI class responsible for showing the interface to
         
         self.folder_frame = Frame()  # The frame that will display the current media folder
         self.var = StringVar()  # The value that stores the current media folder's path as a string
+
         # The label that will display the current media folder
         self.media_folder_label = Label(self.folder_frame, textvariable=self.var)
+
+        # The button that allows the user to change the currently selected media folder
+        self.change_folder_button = Button(self.folder_frame, text="Change...", command=self.folder_selector)
 
         # The frame that will display all the media content available inside the media folder
         self.media_frame = Frame()
@@ -43,6 +53,40 @@ class SongStorage(Tk):  # The GUI class responsible for showing the interface to
         self.add_music_button.pack()
 
         self.process_widgets()
+
+    # This method attempts a connection to the application's database, returning the connection is successful
+    def connect_to_database(self):
+
+        # Attempting a connection to the database
+        try:
+            connection = sqlite3.connect('Resources/media.db')
+        except Error as e:
+            print(e)
+            return
+
+        # The variable storing the SQL command for creating the "media" table
+        create_table = """ CREATE TABLE IF NOT EXISTS media (
+                            id integer PRIMARY KEY,
+                            title text NOT NULL,
+                            artist text NOT NULL,
+                            album text,
+                            release_date DATE,
+                            tags TEXT,
+                            mode BIT default 0 NOT NULL,
+                            full_path text NOT NULL UNIQUE
+                            ); """
+
+        # Attempting to create the "media" table if it doesn't exist already
+        try:
+            cursor = connection.cursor()
+            cursor.execute(create_table)
+            connection.commit()
+            cursor.close()
+        except Error as e:
+            print(e)
+
+        # Returning the connection variable so that it can be used by other features of the application
+        return connection
 
     def process_widgets(self):  # Processes the widgets declared in the initialization method of the class
         self.init_frame.pack()
@@ -61,7 +105,7 @@ class SongStorage(Tk):  # The GUI class responsible for showing the interface to
 
         self.header.pack()
 
-    def folder_selector(self):  # This function prompts the user to select the media folder
+    def folder_selector(self):  # Prompts the user to select the media folder
         folder = filedialog.askdirectory()  # We will use an OS-specific dialog box call to select the media folder
         
         self.config.set('MEDIA FOLDER', 'folder', folder)  # Updating the value inside the configuration file
@@ -71,7 +115,7 @@ class SongStorage(Tk):  # The GUI class responsible for showing the interface to
 
         self.display_media_folder()
         
-    def display_media_folder(self):  # This function makes the media folder label display the correct folder
+    def display_media_folder(self):  # This method makes the media folder label display the correct folder
 
         # Updating the value of the variable storing the path to the media folder
         self.media_folder = self.config['MEDIA FOLDER']['folder']
@@ -79,6 +123,7 @@ class SongStorage(Tk):  # The GUI class responsible for showing the interface to
         if self.media_folder != "":  # Checking if the user has previously selected a media folder
             self.folder_locator.pack_forget()
             self.folder_button.pack_forget()
+            self.change_folder_button.pack(side=LEFT)
 
             # Updating the value of the variable that the media folder label will use
             self.var.set("Media folder: " + self.media_folder)
