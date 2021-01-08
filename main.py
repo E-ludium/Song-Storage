c4b4f01d0f6a1bd4c3e3252c4c0236b707721a9b
E-ludium <48441956+E-ludium@users.noreply.github.com>
2021-01-08 14:27:01 +0200

    Visual improvements + better code readability
    - Improved the graphical interface
    - Added a new method for processing the widgets declared in the initialization method of the application class

--- main.py
+++ main.py
@@ -25,33 +25,42 @@ class SongStorage(Tk):  # The GUI class responsible for showing the interface to
                                     text="Please choose the folder where you'd like to store your media: ")
         # This button will prompt the user to select a media folder
         self.folder_button = Button(self.init_frame, text="Browse...", command=self.folder_selector)
-
-        self.init_frame.pack()
-        self.folder_locator.pack()
-        self.folder_button.pack()
         
         self.folder_frame = Frame()  # The frame that will display the current media folder
         self.var = StringVar()  # The value that stores the current media folder's path as a string
         # The label that will display the current media folder
         self.media_folder_label = Label(self.folder_frame, textvariable=self.var)
 
-        self.folder_frame.pack()
-        self.media_folder_label.pack()
-
         # The frame that will display all the media content available inside the media folder
         self.media_frame = Frame()
-        self.media_frame.pack()
 
         self.path_frame_parent = Frame(self.media_frame, relief=GROOVE, width=500, height=100, bd=1)
-        self.path_frame_parent.pack()
 
         # The button prompting the user to add media content to the media folder
         self.add_music_button = Button(self.path_frame_parent, text="Add Music...")
         self.header = Label(self.media_frame, text="Available media:")
 
         self.add_music_button.pack()
+
+        self.process_widgets()
+
+    def process_widgets(self):  # Processes the widgets declared in the initialization method of the class
+        self.init_frame.pack()
+
+        self.folder_frame.pack()
+
+        self.folder_locator.pack(side=LEFT)
+
+        self.folder_button.pack(side=LEFT)
+
+        self.media_folder_label.pack(side=LEFT)
+
+        self.path_frame_parent.pack(side=LEFT)
+
+        self.media_frame.pack(side=LEFT)
+
         self.header.pack()
-        
+
     def folder_selector(self):  # This function prompts the user to select the media folder
         folder = filedialog.askdirectory()  # We will use an OS-specific dialog box call to select the media folder
         
